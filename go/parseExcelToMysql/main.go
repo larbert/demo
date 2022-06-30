f8becc433cb1424213593f43aa7466a8ac7d53eb
larbert <17835126760@163.com>
2022-06-30 11:44:08 +0800

    完成了解析支付宝账单的功能

--- go/parseExcelToMysql/main.go
+++ go/parseExcelToMysql/main.go
@@ -28,7 +28,7 @@ type bill struct {
 }
 
 func main() {
-	readWeiChatBill()
+	// readWeiChatBill()
 	readAlipayBill()
 }
 
@@ -51,9 +51,8 @@ func readWeiChatBill() {
 		log.Fatal(err)
 	}
 
-
 	// 打开excel文件
-	f, err := excelize.OpenFile("excel1.xlsx")
+	f, err := excelize.OpenFile("weichat.xlsx")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -63,7 +62,7 @@ func readWeiChatBill() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	rows = rows[17:]
+	rows = rows[16:]
 
 	// 解析成bill对象
 	bills := make([]*bill, 0)
@@ -105,10 +104,82 @@ func readWeiChatBill() {
 			log.Fatal(err)
 		}
 		log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
-
 	}
 }
 
 func readAlipayBill() {
+	// 准备数据库连接
+	db, err := sql.Open("mysql", "root:Ltf*ms93@tcp(127.0.0.1:3306)/bill?parseTime=true")  
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+	// 测试数据库连接
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	stmt, err := db.Prepare("INSERT INTO bill(timestamp, in_ex, commodity, commodity_type, ammount, pay_method, counterparty, tran_account, order_number, mer_number, source, remark)VALUES(?,?,?,?,?,?,?,?,?,?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 打开excel文件
+	f, err := excelize.OpenFile("alipay.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 获取 Sheet1 上所有单元格
+	rows, err := f.GetRows("Sheet1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows = rows[2:]
+
+	// 解析成bill对象
+	bills := make([]*bill, 0)
+	for _, row := range rows {
+		log.Println(row[10])
+		tmpTime, err := time.Parse("2006/01/02 15:04:05", row[10])
+		if err != nil {
+			log.Fatal(err)
+		}
+		tmpAmmount, err := decimal.NewFromString(row[5])
+		if err != nil {
+			log.Fatal(err)
+		}
+		tmpBill := &bill{
+			Timestamp: tmpTime,
+			In_ex: row[0],
+			Commodity: row[3],
+			Commodity_type: row[7],
+			Ammount: tmpAmmount,
+			Pay_method: row[4],
+			Counterparty: row[1],
+			Tran_account: row[2],
+			Order_number: row[8],
+			Mer_number: row[9],
+			Source: "支付宝",
+			Remark: "",
+		}
+		bills = append(bills, tmpBill)
+	}
+	for _, b := range bills {
+		res, err := stmt.Exec(b.Timestamp, b.In_ex, b.Commodity, "", b.Ammount, b.Pay_method, b.Counterparty, "", b.Order_number, b.Mer_number, b.Source, b.Remark)
+		if err != nil {
+			log.Fatal(err)
+		}
+		lastId, err := res.LastInsertId()
+		if err != nil {
+			log.Fatal(err)
+		}
+		rowCnt, err := res.RowsAffected()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+	}
 }
